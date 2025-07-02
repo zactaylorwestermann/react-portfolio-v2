c5175323e323cf7f6e7e5c7024f5751065814fed
gummipainkillers <zactaylorwestermann@gmail.com>
2025-07-02 17:54:17 +1000

    updated footer

--- src/pages/home/components/HomeFooter.jsx
+++ src/pages/home/components/HomeFooter.jsx
@@ -1,7 +1,7 @@
 const HomeFooter = () => {
   return (
     <footer className="max-w-md pb-16 text-sm text-p3 sm:pb-0">
-      <p>Perhaps some technologies used and inspirations here</p>
+      <p>Built using React, deployed using Netlify. All font is Roboto.</p>
     </footer>
   );
 };

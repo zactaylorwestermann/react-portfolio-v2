49e8fcb55ca6f773f1d8807ec70c2068b2a55d7d
gummipainkillers <zactaylorwestermann@gmail.com>
2025-07-02 17:54:51 +1000

    Update HomeFooter.jsx

--- src/pages/home/components/HomeFooter.jsx
+++ src/pages/home/components/HomeFooter.jsx
@@ -1,7 +1,7 @@
 const HomeFooter = () => {
   return (
     <footer className="max-w-md pb-16 text-sm text-p3 sm:pb-0">
-      <p>Built using React, deployed using Netlify. All font is Roboto.</p>
+      <p>Coded in Visual Studio, built using React, deployed using Netlify.</p>
     </footer>
   );
 };
